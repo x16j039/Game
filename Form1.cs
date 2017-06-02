42296dd3766f225cc1eee744aea65e65038d93c1
WIN_NT\x16j039 <x16j039@chiba-fjb.ac.jp>
2017-06-02 11:52:47 +0900

    シューティング

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,42 @@ namespace Game
 {
     public partial class Form1 : Form
     {
+        int spd = 10;
+        int moveX;
+        int moveY;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moveY = -spd;
+            moveX = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            moveY = spd;
+            moveX = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            moveX = -spd;
+            moveY = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            moveX = spd;
+            moveY = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += moveX;
+            pictureBox1.Top += moveY;
         }
     }
 }
