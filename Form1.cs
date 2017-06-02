39b5b8ad57e2b78800454f393feaa00b10264730
WIN_NT\x16j039 <x16j039@chiba-fjb.ac.jp>
2017-06-02 12:19:37 +0900

    キーボード

--- Form1.cs
+++ Form1.cs
@@ -20,34 +20,36 @@ namespace Game
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            moveY = -spd;
-            moveX = 0;
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            moveY = spd;
-            moveX = 0;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            moveX = -spd;
-            moveY = 0;
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            moveX = spd;
-            moveY = 0;
-        }
-
         private void timer1_Tick(object sender, EventArgs e)
         {
             pictureBox1.Left += moveX;
             pictureBox1.Top += moveY;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                moveY = -spd;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                moveY = spd;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                moveX = -spd;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveX = spd;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            moveX = 0;
+            moveY = 0;
         }
     }
 }
